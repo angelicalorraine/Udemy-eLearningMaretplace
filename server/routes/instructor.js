06b930ccda79cfa9789d54ba9b81e3b5314ea27c
Angelica Hernandez <gel.lorraine@gmail.com>
2022-04-27 22:46:11 -0700

    Create current instructor route

--- server/routes/instructor.js
+++ server/routes/instructor.js
@@ -4,9 +4,14 @@ const router = express.Router();
 
 import { requireSignin } from "../middlewares";
 
-import { makeInstructor, getAccountStatus } from "../controllers/instructor";
+import {
+  makeInstructor,
+  getAccountStatus,
+  currentInstructor,
+} from "../controllers/instructor";
 
 router.post("/make-instructor", requireSignin, makeInstructor);
 router.post("/get-account-status", requireSignin, getAccountStatus);
+router.get("/current-instructor", requireSignin, currentInstructor);
 
 module.exports = router;
